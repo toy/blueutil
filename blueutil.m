7ced0ff9340dc6ea5da9384c174358ddbec5f941
Ivan Kuchin <ivan@workisfun.ru>
2011-01-29 17:00:35 +0300

    cleanup code

--- blueutil.m
+++ blueutil.m
@@ -3,7 +3,7 @@
  * Command-line utility to control Bluetooth.
  * Uses private API from IOBluetooth framework (i.e. IOBluetoothPreference*()).
  * http://www.frederikseiffert.de/blueutil
- * 
+ *
  * This software is public domain. It is provided without any warranty whatsoever,
  * and may be modified or used without attribution.
  *
@@ -12,42 +12,32 @@
  */
 
 #import <Foundation/Foundation.h>
-// link against IOBluetooth.framework
 
-
-inline int BTPowerState()
-{
+inline int BTPowerState() {
 	return IOBluetoothPreferenceGetControllerPowerState();
 }
 
-int BTSetPowerState(int powerState)
-{
+int BTSetPowerState(int powerState) {
 	IOBluetoothPreferenceSetControllerPowerState(powerState);
-	
+
 	usleep(2000000); // wait until BT has been set
 	if (BTPowerState() != powerState) {
 		printf("Error: unable to turn Bluetooth %s\n", powerState ? "on" : "off");
 		return EXIT_FAILURE;
 	}
-	
+
 	return EXIT_SUCCESS;
 }
 
-void BTStatus()
-{
-	printf("Status: %s\n", BTPowerState() ? "on" : "off");
-}
-
-int main(int argc, const char * argv[])
-{
-    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+int main(int argc, const char * argv[]) {
+	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
 	int result = EXIT_SUCCESS;
 
-    if (!IOBluetoothPreferencesAvailable()) {
+	if (!IOBluetoothPreferencesAvailable()) {
 		printf("Error: Bluetooth not available");
 		result = EXIT_FAILURE;
 	} else if (argc == 2 && strcmp(argv[1], "status") == 0) {
-		BTStatus();
+		printf("Status: %s\n", BTPowerState() ? "on" : "off");
 	} else if (argc == 2 && strcmp(argv[1], "on") == 0) {
 		result = BTSetPowerState(1);
 	} else if (argc == 2 && strcmp(argv[1], "off") == 0) {
@@ -56,7 +46,7 @@ int main(int argc, const char * argv[])
 		printf("Usage: %s [status|on|off]\n", argv[0]);
 		result = EXIT_FAILURE;
 	}
-	
-    [pool release];
-    return result;
+
+	[pool release];
+	return result;
 }
